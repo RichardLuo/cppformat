b71f435c40b7ee912ffac7269c01506f9db5be62
RichardLuo <cdominant7@gmail.com>
2015-06-07 23:16:14 +0800

    added support for arm, added stlport and disabled exceptions

--- Android.mk
+++ Android.mk
@@ -9,11 +9,17 @@ LOCAL_MODULE := libcppformat
 LOCAL_SRC_FILES := format.cc
 
 # LOCAL_CFLAGS += -std=c++11 -fexceptions
-LOCAL_CFLAGS += -fexceptions
+# LOCAL_CFLAGS += -fexceptions
 
 LOCAL_PRELINK_MODULE := false
 
 LOCAL_MODULE_TAGS := eng
+
+ifneq ($(TARGET_SIMULATOR),true)
+LOCAL_C_INCLUDES += bionic		# very important!
+LOCAL_C_INCLUDES += external/stlport/stlport 
+LOCAL_SHARED_LIBRARIES += libstlport libdl
+endif
 
 include $(BUILD_SHARED_LIBRARY)
 include $(LOCAL_PATH)/format_test.mk
--- format_test.mk
+++ format_test.mk
@@ -25,4 +25,10 @@ LOCAL_PRELINK_MODULE := false
 
 LOCAL_MODULE_TAGS := eng
 
+ifneq ($(TARGET_SIMULATOR),true)
+LOCAL_C_INCLUDES += bionic		# very important!
+LOCAL_C_INCLUDES += external/stlport/stlport 
+LOCAL_SHARED_LIBRARIES += libstlport libdl
+endif
+
 include $(BUILD_EXECUTABLE)
