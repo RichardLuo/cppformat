a44c360de611e72823ec1869d692fbcc3828283e
Richard Luo <cdominant7@gmail.com>
2015-09-01 14:49:09 +0800

    ported cppformat to droid jni building

--- Android.mk
+++ Android.mk
@@ -1,15 +1,13 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := cppformat_static
-LOCAL_MODULE_FILENAME := libcppformat
+LOCAL_MODULE := libcppformat
 
 LOCAL_SRC_FILES := format.cc
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_CFLAGS += -fexceptions
 
-LOCAL_CFLAGS += -std=c++11 -fexceptions
+include $(BUILD_SHARED_LIBRARY)
 
-include $(BUILD_STATIC_LIBRARY)
 
